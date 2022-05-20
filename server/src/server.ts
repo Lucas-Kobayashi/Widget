dc34629d03dcf8bcea34d6d5db74090ecc9dcb01
Lucas Kobayashi <kobacarmo2@hotmail.com>
2022-05-20 19:57:18 -0300

    Preparando o envio de email mock

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,11 +7,21 @@
 // DELETE = Apagar uma informação.
 
 import express from "express";
+import nodemailer from "nodemailer";
 import { prisma } from "./prisma";
 
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "2f8e5e612fafae",
+    pass: "c3cb14de8e70d3"
+  }
+});
 
 app.post("/feedbacks", async (req, res) => {
   const { type, comment, screenshot } = req.body;
@@ -22,6 +32,18 @@ app.post("/feedbacks", async (req, res) => {
       comment,
       screenshot
     }
+  });
+
+  await transport.sendMail({
+    from: "Equipe feedget <equipe@feedget.com>",
+    to: "Lucas Kobayashi <kobacarmo2@hotmail.com",
+    subject: "Novo feedback",
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`
+    ].join("\n")
   });
 
   return res.status(201).json({ data: feedback });
