c6011c4d7b55a1cc8814b0e5cd350511bba48d65
Lucas Kobayashi <kobacarmo2@hotmail.com>
2022-05-22 21:10:03 -0300

    Envio de email mock feito

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,47 +7,13 @@
 // DELETE = Apagar uma informação.
 
 import express from "express";
-import nodemailer from "nodemailer";
-import { prisma } from "./prisma";
+import { routes } from "./route";
 
 const app = express();
 
 app.use(express.json());
 
-const transport = nodemailer.createTransport({
-  host: "smtp.mailtrap.io",
-  port: 2525,
-  auth: {
-    user: "2f8e5e612fafae",
-    pass: "c3cb14de8e70d3"
-  }
-});
-
-app.post("/feedbacks", async (req, res) => {
-  const { type, comment, screenshot } = req.body;
-
-  const feedback = await prisma.feedback.create({
-    data: {
-      type,
-      comment,
-      screenshot
-    }
-  });
-
-  await transport.sendMail({
-    from: "Equipe feedget <equipe@feedget.com>",
-    to: "Lucas Kobayashi <kobacarmo2@hotmail.com",
-    subject: "Novo feedback",
-    html: [
-      `<div style="font-family: sans-serif; font-size: 16px; color: #111">`,
-      `<p>Tipo do feedback: ${type}</p>`,
-      `<p>Comentário: ${comment}</p>`,
-      `</div>`
-    ].join("\n")
-  });
-
-  return res.status(201).json({ data: feedback });
-});
+app.use(routes);
 
 app.listen(3333, () => {
   console.log("Tá rodando o sever");
